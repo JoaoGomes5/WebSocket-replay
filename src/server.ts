9c0acabf52e3c346ecd8cdecd40edc8804698021
João Gomes <joaopfg.2002@gmail.com>
2020-12-17 16:58:26 +0000

    feat: initial server structure

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 import '@controllers/UsersController'
+import WebSocket from 'ws'
+import http from 'http'
 
 const app = express()
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server: server })
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' })
